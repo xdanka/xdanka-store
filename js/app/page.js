077bc098893d15426ec1a16f575aeb03c13f6733
xdanka <atendimento.xdanka@gmail.com>
2026-07-16 22:11:48 -0300

    Add Home component for main page

---
+++ js/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>XDANKA Materiais de Construção</h1>
+      <p>
+        Cerâmicas, porcelanatos, revestimentos e materiais para sua obra.
+      </p>
+    </main>
+  );
+}
